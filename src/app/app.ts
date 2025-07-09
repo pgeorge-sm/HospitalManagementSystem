3b7ed4315f6ca3a5f91dacaf6e4217bca1310959
pgeorge-ace <preethageorge.acesoft@gmail.com>
2025-07-09 12:35:40 +0530

    Initial Commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, 
+    FormsModule,
+    CommonModule  
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected title = 'HospitalManagementSystem';
+  someValue: string = "";
+  someValues: Array<string> = new Array<string>();
+  CallSomeLogic(){
+    this.someValues.push(this.someValue);
+    this.someValue='';
+  }
 }
